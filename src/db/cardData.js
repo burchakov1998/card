849db7a94faefe5d84d20bb782f0f704b4b66901
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-09-18 16:07:18 +0300

    modifed

--- src/db/cardData.js
+++ src/db/cardData.js
@@ -1,6 +1,6 @@
 const data = [
-     {"title": "Card title","text": "Some quick example text to build on the card title and make up the buik of the card's content", "url":"https://avatars.mds.yandex.net/get-zen_doc/4519444/pub_60c028d7cc10d27a0d390ec4_60c02ce43d5d2a1750260641/scale_1200" },
-     {"title": "Card title","text": "Some quick example text to build on the card title and make up the buik of the card's content"},
+     {"title": "Card title","text": "Some quick example text to build on the card title and make up the buik of the card's content", "url":"https://rn-l.imgix.net/images/nopic.jpg?auto=compress%2Cformat&crop=focalpoint&fit=crop&fm=jpg&fp-x=0.5&fp-y=0.5&h=4134&q=70&w=3800&s=fccdee6ceaaa983620dd0deb09f12278" },
+     // {"title": "Card title","text": "Some quick example text to build on the card title and make up the buik of the card's content"},
      {"title": "Special title treatment","text": "With supporting text below as a natural lead-in to additional content","url":"https://avatanplus.com/files/resources/original/57b949bd361d2156abc80b26.jpg" },
 
 ]
